17e865e42774e7fcf5e2fb8198a2f5879de5d1c6
Amr <Amr@Amr-PC>
2016-03-10 06:07:37 -0800

    new sign in class controller

--- AgriMarket/src/java/model/DAO/UserDao.java
+++ AgriMarket/src/java/model/DAO/UserDao.java
@@ -15,7 +15,7 @@ import model.pojo.User;
 public interface UserDao {
     
    public List<User> getAllUsers();
-   public User signIn(String email);
+   public User signIn(String email,String Password);
    public void updateUser(User user);
    public boolean signUp(User user);
 }
